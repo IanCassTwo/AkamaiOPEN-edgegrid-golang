4ac56272c7e1072780b73b01dca1d0d90696017c
Ian Cass <icass@akamai.com>
2020-08-25 11:22:26 +0100

    add active firings alert api

---
+++ alerts-v2/alerts.go
@@ -0,0 +1,55 @@
+package alerts
+
+import (
+	"time"
+	client "github.com/akamai/AkamaiOPEN-edgegrid-golang/client-v1"
+)
+
+type Firing struct {
+	FiringID     string      `json:"firingId"`
+	DefinitionID string      `json:"definitionId"`
+	Name         string      `json:"name"`
+	StartTime    time.Time   `json:"startTime"`
+	EndTime      interface{} `json:"endTime"`
+	Service      string      `json:"service"`
+	FieldMap     struct {
+		AlertType string `json:"alertType"`
+		Zone      string `json:"zone"`
+		EmailTo   string `json:"emailTo"`
+	} `json:"fieldMap"`
+}
+
+
+type FiringsList struct {
+	Firings []Firing `json:"data"`
+}
+
+func ListActiveFirings() (*FiringsList, error) {
+	req, err := client.NewRequest(
+		Config,
+		"GET",
+		"/alerts/v2/alert-firings/active",
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.Do(Config, req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if client.IsError(res) {
+		return nil, client.NewAPIError(res)
+	}
+
+	var response FiringsList
+	if err = client.BodyJSON(res, &response); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
